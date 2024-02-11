85e6b5d7f5637ad9d5c3964b4b726618ad8b83dd
3lsy <echavez-@student.42.fr>
2024-02-11 16:38:21 +0100

    correction in function announce()

--- module01/ex00/Zombie.cpp
+++ module01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/09 18:36:58 by echavez-          #+#    #+#             */
-/*   Updated: 2024/02/10 00:14:31 by echavez-         ###   ########.fr       */
+/*   Updated: 2024/02/11 16:35:46 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ Zombie::Zombie(std::string name)
 
 Zombie::~Zombie(void)
 {
-	std::cout << "<" << this->name << "> has been destroyed" << std::endl;
+	std::cout << this->name << " has been destroyed" << std::endl;
 }
 
 void	Zombie::announce(void)
 {
-	std::cout << "<" << this->name << "> BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
