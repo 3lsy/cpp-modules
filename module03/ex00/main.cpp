64fa4370e8b215d402f5b239feb5a12583fd2d15
3lsy <echavez-@student.42.fr>
2024-03-02 11:35:05 +0100

    module03: ex00 updated

--- module03/ex00/main.cpp
+++ module03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 13:30:40 by echavez-          #+#    #+#             */
-/*   Updated: 2024/03/01 16:50:54 by echavez-         ###   ########.fr       */
+/*   Updated: 2024/03/02 11:29:23 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,12 +46,26 @@ int main() {
     clap1.beRepaired(3);
     clap2.beRepaired(7);
 
+	//Test assignment operator
     std::cout << std::endl << "Assigning ClapTrap objects:" << std::endl;
 	std::cout << "clap1 before assignment: " << clap1.getName() << ", HP: " << clap1.getHitPoints() << ", Energy: " << clap1.getEnergyPoints() << ", Attack Damage: " << clap1.getAttackDamage() << std::endl;
 	std::cout << "clap2 before assignment: " << clap2.getName() << ", HP: " << clap2.getHitPoints() << ", Energy: " << clap2.getEnergyPoints() << ", Attack Damage: " << clap2.getAttackDamage() << std::endl;
     clap1 = clap2;
 	std::cout << "clap1 after assignment: " << clap1.getName() << ", HP: " << clap1.getHitPoints() << ", Energy: " << clap1.getEnergyPoints() << ", Attack Damage: " << clap1.getAttackDamage() << std::endl;
 	std::cout << "clap2 after assignment: " << clap2.getName() << ", HP: " << clap2.getHitPoints() << ", Energy: " << clap2.getEnergyPoints() << ", Attack Damage: " << clap2.getAttackDamage() << std::endl;
+
+
+    // Test copy constructor
+	std::cout << "-----------------------------------------" << std::endl;
+	std::cout << "Creating ClapTrap using copy constructor:" << std::endl;
+	std::cout << "-----------------------------------------" << std::endl;
+	ClapTrap clapTrap3(clap2);
+	std::cout << "clap2 attributes: Name: " << clap2.getName() << ", HP: " << clap2.getHitPoints() << ", Energy: " << clap2.getEnergyPoints() << ", Attack Damage: " << clap2.getAttackDamage() << std::endl;
+	std::cout << "clap3 attributes: Name: " << clapTrap3.getName() << ", HP: " << clapTrap3.getHitPoints() << ", Energy: " << clapTrap3.getEnergyPoints() << ", Attack Damage: " << clapTrap3.getAttackDamage() << std::endl;
+	clapTrap3.attack("Target");
+	clapTrap3.takeDamage(30);
+	clapTrap3.beRepaired(15);
+	std::cout << "clap3 after fight: Name: " << clapTrap3.getName() << ", HP: " << clapTrap3.getHitPoints() << ", Energy: " << clapTrap3.getEnergyPoints() << ", Attack Damage: " << clapTrap3.getAttackDamage() << std::endl;
 
 	std::cout << std::endl << "ClapTrap objects after further modifications:" << std::endl;
     clap1.attack("New Target");
