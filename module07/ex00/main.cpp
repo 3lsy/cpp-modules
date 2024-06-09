95c64ea4ef73819e06b18279857c2ea42fc080f6
3lsy <echavez-@student.42.fr>
2024-06-09 13:01:44 +0200

    mod07: ex00: added :: when calling function template in the main

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 12:18:07 by echavez-          #+#    #+#             */
-/*   Updated: 2024/06/09 12:49:42 by echavez-         ###   ########.fr       */
+/*   Updated: 2024/06/09 12:58:57 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ int main() {
 	int x = 10, y = 20;
 
 	std::cout << "Before swap: x = " << x << ", y = " << y << std::endl;
-    swap(x, y);
+    ::swap(x, y);
     std::cout << "After swap: x = " << x << ", y = " << y << std::endl;
 
-	std::cout << "min(x, y) = " << min(x, y) << std::endl;
-    std::cout << "max(x, y) = " << max(x, y) << std::endl;
+	std::cout << "min(x, y) = " << ::min(x, y) << std::endl;
+    std::cout << "max(x, y) = " << ::max(x, y) << std::endl;
 
 	std::cout << "--------------------------------" << std::endl;
 
 	std::string c = "string1", d = "string2";
 
 	std::cout << "Before swap: c = " << c << ", d = " << d << std::endl;
-    swap(x, y);
+    ::swap(c, d);
     std::cout << "After swap: c = " << c << ", d = " << d << std::endl;
 
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
@@ -39,11 +39,11 @@ int main() {
 	int a = 1, b = 1;
 
 	std::cout << "Before swap: a = " << a << ", b = " << b << std::endl;
-    swap(a, b);
+    ::swap(a, b);
     std::cout << "After swap: a = " << a << ", b = " << b << std::endl;
 
-	std::cout << "min(a, b) = " << min(a, b) << std::endl;
-    std::cout << "max(a, b) = " << max(a, b) << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 
 	return 0;
 }
