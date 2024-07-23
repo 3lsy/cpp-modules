bb16963986616aa8b995997f9e1b939b999b1af6
3lsy <echavez-@student.42.fr>
2024-07-23 13:02:43 +0200

    cpp07:ex00, more test

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 12:18:07 by echavez-          #+#    #+#             */
-/*   Updated: 2024/06/09 12:58:57 by echavez-         ###   ########.fr       */
+/*   Updated: 2024/07/23 12:59:04 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,40 @@ int main() {
 
 	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
     std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	std::cout << "--------------------------------" << std::endl;
+    int arr1[5] = {5, 2, 9, 1, 7};
+    int arr2[5] = {3, 8, 6, 4, 0};
+
+    // Demonstrate swap of whole arrays
+    std::cout << "Before swap:" << std::endl;
+    std::cout << "arr1: ";
+    for (int i = 0; i < 5; ++i) std::cout << arr1[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "arr2: ";
+    for (int i = 0; i < 5; ++i) std::cout << arr2[i] << " ";
+    std::cout << std::endl;
+
+    for (int i = 0; i < 5; ++i) {
+        swap(arr1[i], arr2[i]);
+    }
+
+    std::cout << "After swap:" << std::endl;
+    std::cout << "arr1: ";
+    for (int i = 0; i < 5; ++i) std::cout << arr1[i] << " ";
+    std::cout << std::endl;
+
+    std::cout << "arr2: ";
+    for (int i = 0; i < 5; ++i) std::cout << arr2[i] << " ";
+    std::cout << std::endl;
+
+    // Demonstrate min and max on swapped arrays
+    int minVal = min(arr1[0], arr2[0]);
+    std::cout << "Minimum of arr1[0] and arr2[0] is: " << minVal << std::endl;
+
+    int maxVal = max(arr1[4], arr2[4]);
+    std::cout << "Maximum of arr1[4] and arr2[4] is: " << maxVal << std::endl;
 
 	return 0;
 }
