9e6b6e810bce8622cd4d0ff8b52664c749a4e6f0
3lsy <echavez-@student.42.fr>
2024-07-24 11:31:09 +0200

    cpp08: test updated

--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 17:25:24 by echavez-          #+#    #+#             */
-/*   Updated: 2024/06/10 10:46:42 by echavez-         ###   ########.fr       */
+/*   Updated: 2024/07/24 11:28:46 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,38 @@
 #include <list>
 #include "easyfind.hpp"
 
+// Templated function to print elements of any container
+template <typename Container>
+void printContainer(const Container& container) {
+    std::cout << "Container elements: {";
+    for (typename Container::const_iterator it = container.begin(); it != container.end(); ++it) {
+        std::cout << *it << ", ";
+	}
+	std::cout << "}";
+	std::cout << std::endl;
+}
+
 int main() {
+	//vector declaration
 	std::vector<int>::iterator it;
-    std::vector<int> v = {1, 2, 3, 4, 5};
+	std::vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+	//list declaration
 	std::list<int>::iterator it2;
-	std::list<int> l = {1, 2, 3, 4, 5};
+	std::list<int> l;
+	l.push_back(1);
+    l.push_back(2);
+    l.push_back(3);
+    l.push_back(4);
+    l.push_back(5);
 
 	std::cout << "----- VECTOR TESTS -----" << std::endl;
 
+	printContainer(v);
     try {
         it = easyfind(v, 3);
         std::cout << "[vector] Found: " << *it << std::endl;
@@ -37,6 +61,7 @@ int main() {
 
 	std::cout << "----- LIST TESTS -----" << std::endl;
 
+	printContainer(l);
 	try {
 		it2 = easyfind(l, 3);
 		std::cout << "[list] Found: " << *it2 << std::endl;
