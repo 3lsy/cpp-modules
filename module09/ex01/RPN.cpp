2b34239ba6bdec55a8f76d29511608ecd82fc751
3lsy <echavez-@student.42.fr>
2024-09-05 17:06:00 +0200

    rm comments

--- module09/ex01/RPN.cpp
+++ module09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: echavez- <echavez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 12:50:48 by echavez-          #+#    #+#             */
-/*   Updated: 2024/06/12 17:42:10 by echavez-         ###   ########.fr       */
+/*   Updated: 2024/09/05 16:19:38 by echavez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ RPN::RPN(const RPN &src) {
 
 RPN &RPN::operator=(const RPN &src) {
 	(void)src;
-	return *this;
+	return (*this);
 }
 
 bool RPN::isOperator(const std::string& token) {
@@ -79,40 +79,6 @@ void RPN::evaluate(const std::string& expression) {
 	}
 	std::cout << stack.top() << std::endl;
 }
-
-/*
-void RPN::evaluate(const std::string& expression) {
-    std::stack<int> stack;
-    std::istringstream stream(expression);
-    std::string token;
-
-	std::cout << "Expression: " << expression << std::endl;
-    while (stream >> token) {
-		std::cout << "Token: " << token << std::endl;
-        if (isOperator(token)) {
-            if (stack.size() < 2) {
-                throw NotEnoughOperands();
-            }
-            int b = stack.top(); stack.pop();
-            int a = stack.top(); stack.pop();
-            stack.push(performOperation(token, a, b));
-        } else {
-            int number = std::atoi(token.c_str());
-			std::cout << "Number: " << number << std::endl;
-            if (number > 10) {
-				//print number
-				std::cout << "Number: " << number << std::endl;
-                throw std::runtime_error("Error: numeric argument greater than 10 is not valid");
-            }
-            stack.push(number);
-        }
-    }
-    if (stack.size() != 1) {
-        throw InvalidExpression();
-    }
-	std::cout << stack.top() << std::endl;
-}
-*/
 
 //exceptions
 const char* RPN::InvalidExpression::what() const throw() {
